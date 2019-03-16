ac39e77952cc75ab679765c7eb5f02d1932d64e5
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2019-03-16 15:39:58 +0000

    Created 2nd branch for different temperature rescale implementation. This one will allow us to set a desired T0 and gamma, and will calculate the T0 and gamma rescaling factors to abstractsnapshot classes

--- reinstall.sh
+++ reinstall.sh
@@ -1,5 +1,5 @@
 echo Removing old install
-rm -r ~/.local/lib/python3.5/site-packages/fake_spectra*
+rm -r ~/.local/lib/python3.6/site-packages/fake_spectra*
 rm -r build/*
 echo Installing new version
 python3 setup.py build
