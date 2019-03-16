82438bed2dd32c7160ac2a8670895f0309d2ea39
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2019-03-16 19:06:40 +0000

    Removed something hardcoded for debugging

--- fake_spectra/griddedspectra.py
+++ fake_spectra/griddedspectra.py
@@ -10,10 +10,9 @@ class GriddedSpectra(spectra.Spectra):
     """Generate metal line spectra from simulation snapshot. Default parameters are BOSS DR9"""
     def __init__(self,num, base, nspec=200, res = 90., savefile="gridded_spectra.hdf5", reload_file=True, set_T0=None, set_gamma=None, **kwargs):
         # get box size from file (either HDF5 or BigFile
-        #f = absn.AbstractSnapshotFactory(num, base, Tscale=1, gammascale=1)
-        #self.box = f.get_header_attr("BoxSize")
-        self.box=60000.0
-        #del f
+        f = absn.AbstractSnapshotFactory(num, base, Tscale=1, gammascale=1)
+        self.box = f.get_header_attr("BoxSize")
+        del f
         self.NumLos = nspec*nspec
         #All through y axis
         axis = np.ones(self.NumLos)
