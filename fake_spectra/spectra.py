2b0bbd1764e480a959a99373be4b84da2347609a
Keir <keirrogers@hotmail.com>
2020-07-21 13:27:37 +0100

    Fix mean flux

--- fake_spectra/spectra.py
+++ fake_spectra/spectra.py
@@ -799,12 +799,15 @@ class Spectra(object):
 
     def add_tau_z_evolution(self, tau, tau_effective_model=tau_effective_Boera):
         """Correct optical depths for specified redshift evolution."""
-        tau_mean = np.mean(tau)
+        #tau_mean = np.mean(tau)
         delta_z = self.vmax / 3.e+5
         z_vector = np.linspace(start=0, stop=delta_z, num=tau.shape[-1] + 1)[:-1]
         z_vector += (self.red - (delta_z / 2.))
-        tau *= tau_effective_model(z_vector)[np.newaxis, :] / tau_mean
-        return tau
+        flux = np.exp(-1. * tau)
+        flux_mean = np.mean(flux)
+        flux *= np.exp(-1. * tau_effective_model(z_vector)[np.newaxis, :]) / flux_mean
+        #tau *= tau_effective_model(z_vector)[np.newaxis, :] / tau_mean
+        return -1. * np.log(flux)
 
     def _vel_single_file(self,fn, elem, ion):
         """Get the column density weighted interpolated velocity field for a single file"""
